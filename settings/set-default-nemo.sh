c1c48a2cd73222062e963df972160793253a32b8
shimarulin <shimarulin@gmail.com>
2014-06-19 10:46:19 +0400

    Add setting - Nemo set default filemanager

---
+++ settings/set-default-nemo.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+xdg-mime default nemo.desktop inode/directory application/x-gnome-saved-search
+gsettings set org.gnome.desktop.background show-desktop-icons false
+gsettings set org.nemo.desktop show-desktop-icons true
