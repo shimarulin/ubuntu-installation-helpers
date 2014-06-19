7b62cc6f39d90be8cf16962486b366425a4f5699
shimarulin <shimarulin@gmail.com>
2014-06-19 17:43:52 +0400

    install -y

--- install/mongodb.sh
+++ install/mongodb.sh
@@ -4,4 +4,4 @@ DIST=${DISTRIB_CODENAME}
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart ${DIST} 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
 sudo apt-get update
-sudo apt-get install mongodb-org
+sudo apt-get install -y mongodb-org
--- install/virtualbox.sh
+++ install/virtualbox.sh
@@ -4,4 +4,4 @@ DIST=${DISTRIB_CODENAME}
 wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | sudo apt-key add -
 echo 'deb http://download.virtualbox.org/virtualbox/debian ${DIST} contrib non-free' | sudo tee /etc/apt/sources.list.d/virtualbox.list
 sudo apt-get update
-sudo apt-get install virtualbox-4.3
+sudo apt-get install -y virtualbox-4.3
