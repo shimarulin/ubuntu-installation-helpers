301c3cb42b79db09d2f5d0e02aa19103be20ac83
Vyacheslav Shimarulin <shimarulin@gmail.com>
2014-06-21 01:18:32 +0400

    Change symlink to package

--- installers/nodejs.sh
+++ installers/nodejs.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install -y nodejs npm
+sudo apt-get install -y nodejs npm nodejs-legacy
 
 npm_tmp=~/.tmp
 mkdir ${npm_tmp}
 sleep 0.05s # Hack
 bash ../helpers/add-permissions.sh ${npm_tmp}
 npm config set tmp ${npm_tmp}
-sudo ln -s '/usr/bin/nodejs' '/usr/local/bin/node'
+# sudo ln -s '/usr/bin/nodejs' '/usr/local/bin/node' # or install nodejs-legacy
