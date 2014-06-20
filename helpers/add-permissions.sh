d687a5f3c49fb9825961049d5f4a981f9bef13e5
Vyacheslav Shimarulin <shimarulin@gmail.com>
2014-06-20 13:41:40 +0400

    Add sudo for working in root user dir

--- helpers/add-permissions.sh
+++ helpers/add-permissions.sh
@@ -12,16 +12,16 @@ path=$1
 echo "User $user in group ${usergroup}. Target: ${path}"
 
 # Default ACL section
-setfacl -m "default:user::rwx" ${path}
-setfacl -m "default:user:$user:rwx" ${path}
-setfacl -m "default:group::r-x" ${path}
-setfacl -m "default:group:${usergroup}:r-x" ${path}
-setfacl -m "default:other::---" ${path}
-setfacl -m "default:mask::rwx" ${path}
+sudo setfacl -m "default:user::rwx" ${path}
+sudo setfacl -m "default:user:$user:rwx" ${path}
+sudo setfacl -m "default:group::r-x" ${path}
+sudo setfacl -m "default:group:${usergroup}:r-x" ${path}
+sudo setfacl -m "default:other::---" ${path}
+sudo setfacl -m "default:mask::rwx" ${path}
 
 # User ACL section
-setfacl -m "u:$user:rwx" ${path}
-setfacl -m "g:${usergroup}:r-x" ${path}
-setfacl -m "other::---" ${path}
-setfacl -m "mask::rwx" ${path}
+sudo setfacl -m "u:$user:rwx" ${path}
+sudo setfacl -m "g:${usergroup}:r-x" ${path}
+sudo setfacl -m "other::---" ${path}
+sudo setfacl -m "mask::rwx" ${path}
 
