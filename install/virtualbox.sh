831dd00a51ba06bac571b8b855cf94ec5735eb33
Vyacheslav Shimarulin <shimarulin@gmail.com>
2014-06-20 11:12:03 +0400

    Fixed installer virtualbox. Test passed

--- install/virtualbox.sh
+++ install/virtualbox.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 . /etc/lsb-release
-DIST=${DISTRIB_CODENAME}
+dist=${DISTRIB_CODENAME}
 wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | sudo apt-key add -
-echo 'deb http://download.virtualbox.org/virtualbox/debian ${DIST} contrib non-free' | sudo tee /etc/apt/sources.list.d/virtualbox.list
+echo "deb http://download.virtualbox.org/virtualbox/debian ${dist} contrib non-free" | sudo tee /etc/apt/sources.list.d/virtualbox.list
 sudo apt-get update
 sudo apt-get install -y virtualbox-4.3
