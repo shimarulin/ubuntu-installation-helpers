aaaf0d216e0229fecb2ce01bb8813875184cfb45
shimarulin <shimarulin@gmail.com>
2014-06-24 11:14:48 +0400

    Add recursive options

--- helpers/add-permissions.sh
+++ helpers/add-permissions.sh
@@ -12,16 +12,16 @@ path=$1
 echo "User $user in group ${usergroup}. Target: ${path}"
 
 # Default ACL section
-sudo setfacl -m "default:user::rwx" ${path}
-sudo setfacl -m "default:user:$user:rwx" ${path}
-sudo setfacl -m "default:group::r-x" ${path}
-sudo setfacl -m "default:group:${usergroup}:r-x" ${path}
-sudo setfacl -m "default:other::---" ${path}
-sudo setfacl -m "default:mask::rwx" ${path}
+sudo setfacl -R -m "default:user::rwx" ${path}
+sudo setfacl -R -m "default:user:$user:rwx" ${path}
+sudo setfacl -R -m "default:group::r-x" ${path}
+sudo setfacl -R -m "default:group:${usergroup}:r-x" ${path}
+sudo setfacl -R -m "default:other::---" ${path}
+sudo setfacl -R -m "default:mask::rwx" ${path}
 
 # User ACL section
-sudo setfacl -m "u:$user:rwx" ${path}
-sudo setfacl -m "g:${usergroup}:r-x" ${path}
-sudo setfacl -m "other::---" ${path}
-sudo setfacl -m "mask::rwx" ${path}
+sudo setfacl -R -m "u:$user:rwx" ${path}
+sudo setfacl -R -m "g:${usergroup}:r-x" ${path}
+sudo setfacl -R -m "other::---" ${path}
+sudo setfacl -R -m "mask::rwx" ${path}
 
